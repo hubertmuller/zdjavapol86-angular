9935c1ee519ac40b18a5e8973173207e21719644
Hubert Muller <hubert@sportport.eu>
2021-08-26 20:42:01 +0200

    routing 3

---
+++ src/app/dane.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DaneService {
+
+  constructor() { }
+
+  pobierzOsoby(): void {
+
+  }
+}
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DaneService} from '../dane.service';
 
 @Component({
   selector: 'app-lista',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DaneService) {
+    ds.pobierzOsoby();
+  }
 
   ngOnInit(): void {
   }
